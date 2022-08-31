92e838f6d8c379302ff0eba51464cca12200459b
vishal-codes <itsvishal2417@gmail.com>
2022-08-31 18:09:13 +0530

    Setting up the project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = () => {
+    return (
+        <div className='App'>
+            <p>abcdefghijklmnopqrstuvwxyz</p>
+        </div>
+    );
+};
+
+export default App;

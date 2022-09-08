a5627fe3e93ea2afdc3d0e7c1a0b8932316f8e21
vishal-codes <itsvishal2417@gmail.com>
2022-09-08 22:18:14 +0530

    testing
    Signed-off-by: vishal-codes <itsvishal2417@gmail.com>

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -102,24 +102,7 @@ export default function PermanentDrawerLeft({ themeChange, mode }) {
                 <Toolbar sx={{ height: '70px' }} />
                 <Divider />
                 <Box sx={{ p: 3 }}>
-                    <Typography paragraph>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                        sed do eiusmod tempor incididunt ut labore et dolore
-                        magna aliqua. Rhoncus dolor purus non enim praesent
-                        elementum facilisis leo vel. Risus at ultrices mi tempus
-                        imperdiet. Semper risus in hendrerit gravida rutrum
-                        quisque non tellus. Convallis convallis tellus id
-                        interdum velit laoreet id donec ultrices. Odio morbi
-                        quis commodo odio aenean sed adipiscing. Amet nisl
-                        suscipit adipiscing bibendum est ultricies integer quis.
-                        Cursus euismod quis viverra nibh cras. Metus vulputate
-                        eu scelerisque felis imperdiet proin fermentum leo.
-                        Mauris commodo quis imperdiet massa tincidunt. Cras
-                        tincidunt lobortis feugiat vivamus at augue. At augue
-                        eget arcu dictum varius duis at consectetur lorem. Velit
-                        sed ullamcorper morbi tincidunt. Lorem donec massa
-                        sapien faucibus et molestie ac.
-                    </Typography>
+                    <Typography paragraph>Testing some changes</Typography>
                     <Typography paragraph>
                         Consequat mauris nunc congue nisi vitae suscipit.
                         Fringilla est ullamcorper eget nulla facilisi etiam

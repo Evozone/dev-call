d0a3de03e547cd89b477a8683fdae26aeb22892e
The Brahmnic Boy <thebrahmnicboy@gmail.com>
2022-12-25 22:31:43 +0530

    Landing page and test

--- client/src/LandingPage.js
+++ client/src/LandingPage.js
@@ -7,21 +7,46 @@ import Typography from '@mui/material/Typography';
 import ThemeProvider from '@mui/material/styles/ThemeProvider';
 import createTheme from '@mui/material/styles/createTheme';
 import Link from '@mui/material/Link';
+import IconButton from '@mui/material/IconButton';
 import GoogleOneTapLogin from './components/GoogleOneTapLogIn';
+
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
 import { customGlobalScrollBars } from './components/CustomGlobalCSS';
 import { TypewriterCycle } from './components/TypewriterCycle';
+import FeatureCard from './components/FeatureCard';
 
 import ScrollToColor from './components/ScrollToColor';
 
-// Create a list of all the sections on the page
-const sections = [
-    { title: 'Features', url: '#features' },
-    { title: 'Contact', url: '#contact' },
-];
-
 const LandingPage = () => {
     const theme = createTheme();
+
+    // Create a list of all the sections on the page
+    const sections = [
+        { title: 'Features', url: '#features' },
+        { title: 'Contact', url: '#contact' },
+    ];
+
+    // Create a list of all the panels for the features section
+    const panelData = [
+        {
+            image: `/panel1.jpg`,
+            title: `Chat with rich Images and GIFs`,
+            description: `This chat feature allows you to add an extra level of excitement to your conversations by sharing images and GIFs directly within the chat window. You can also easily render images from links, bringing even more visual content to your chats. With this feature, you'll be able to easily add a splash of color and personality to your conversations and make them more engaging and memorable.`,
+        },
+        {
+            image: `/panel2.jpg`,
+            title: `Collaborative Workspaces`,
+            description: `Our Workspaces feature is the perfect tool for teams who need to work together efficiently and effectively. It includes a built-in code-editor and IDE, a whiteboard, and a voice call function, all within the same workspace. No more juggling multiple tabs or apps - with Workspaces, everything you need for successful teamwork is just a few clicks away. Whether you're coding, brainstorming, or holding a meeting, our Workspaces have you covered.`,
+        },
+        {
+            image: `/panel3.jpg`,
+            title: `Meetings from anywhere`,
+            description: `Our platform offers separate video conferencing capabilities for when you need to share your screen or webcam with others. Whether you're holding a presentation or just want to show a colleague something on your computer, our video conferencing feature has you covered. With screenshare and webcam sharing options available, you can easily stay connected with your team no matter where you are.`,
+        },
+    ];
+
 
     return (
         <ThemeProvider theme={theme}>
@@ -248,27 +273,28 @@ const LandingPage = () => {
                     id="features"
                     sx={{
                         width: '100%',
-                        height: '100vh',
+                        height: 'auto',
+                        padding: '2rem 0',
                         display: 'flex',
                         flexDirection: 'column',
-                        justifyContent: 'center',
+                        justifyContent: 'flex-start',
                         alignItems: 'center',
-                        color: '#0288d1',
+                        color: '#03256c',
                         background: 'whitesmoke',
                     }}
                 >
-                    <Typography
-                        variant="h2"
-                        component="div"
-                        sx={{
-                            fontFamily: 'Work Sans', fontWeight: '400',
-                        }}
-                    >
-                        Features
-                    </Typography>
+                    {panelData.map(({ image, title, description }, index) => (
+                        <FeatureCard
+                            image={image}
+                            title={title}
+                            description={description}
+                            index={index}
+                            key={index}
+                        />
+                    ))}
                 </Box>
 
-                {/* Section : Contact and Footer */}
+                {/* Section : Contact */}
                 <Box
                     id="contact"
                     sx={{
@@ -276,25 +302,245 @@ const LandingPage = () => {
                         height: 'auto',
                         display: 'flex',
                         flexDirection: 'column',
-                        justifyContent: 'center',
-                        alignItems: 'center',
                         padding: '2rem 0',
-                        background: '#0288d1',
+                        background: 'linear-gradient(116.82deg, #0288d1 0%, #1976d2 100%)',
                         color: 'whitesmoke',
                     }}
                 >
-                    <Typography
-                        variant="h2"
-                        component="div"
+                    <Box
                         sx={{
-                            fontFamily: 'Work Sans', fontWeight: '400',
+                            width: '90%',
+                            height: 'auto',
+                            display: 'flex',
+                            flexDirection: 'row',
+                            justifyContent: 'center',
+                            alignItems: 'center',
+                            margin: '0 auto',
+                            pb: '1rem',
+                            '@media (max-width:600px)': {
+                                flexDirection: 'column',
+                            },
                         }}
                     >
-                        Contact
-                    </Typography>
+                        {/* For Person 1 */}
+                        <Box
+                            sx={{
+                                width: '50%',
+                                height: 'auto',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                margin: '0 auto',
+                                '@media (max-width:600px)': {
+                                    width: '100%',
+                                },
+                            }}
+                        >
+                            {/* Box with 90% size of parent, rounded corners */}
+                            <Box
+                                sx={{
+                                    width: '90%',
+                                    height: '90%',
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    justifyContent: 'flex-start',
+                                    alignItems: 'center',
+                                    background: '#001126ee',
+                                    borderRadius: 3,
+                                    padding: '1rem',
+                                }}
+                            >
+                                <Typography
+                                    variant='h6'
+                                    component='div'
+                                    sx={{
+                                        fontFamily: 'Work Sans',
+                                        fontWeight: '400',
+                                        margin: '0 1rem',
+                                    }}
+                                >
+                                    Vishal Shinde
+                                </Typography>
+                                <IconButton
+                                    href="https://github.com/your-github-username"
+                                    target="_blank"
+                                    sx={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        color: 'white',
+                                        fontSize: '3rem',
+                                        margin: '0 0.5rem',
+                                        transition: 'all 0.2s ease-in-out',
+                                        '&:hover': {
+                                            transform: 'scale(1.2)',
+                                        },
+                                    }}
+                                >
+                                    <GitHubIcon fontSize="large" />
+                                </IconButton>
+                                <IconButton
+                                    href="https://linkedin.com/in/your-linkedin-username"
+                                    target="_blank"
+                                    sx={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        color: 'white',
+                                        fontSize: '3rem',
+                                        margin: '0 0.5rem',
+                                        transition: 'all 0.2s ease-in-out',
+                                        '&:hover': {
+                                            transform: 'scale(1.2)',
+                                        },
+                                    }}
+                                >
+                                    <LinkedInIcon fontSize="large" />
+                                </IconButton>
+
+                            </Box>
+                        </Box>
+                        {/* For Person 2 */}
+                        <Box
+                            sx={{
+                                width: '50%',
+                                height: 'auto',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                justifyContent: 'center',
+                                alignItems: 'center',
+                                margin: '0 auto',
+                                '@media (max-width:600px)': {
+                                    width: '100%',
+                                },
+                            }}
+                        >
+                            {/* Box with 90% size of parent, rounded corners */}
+                            <Box
+                                sx={{
+                                    width: '90%',
+                                    height: '90%',
+                                    display: 'flex',
+                                    flexDirection: 'row',
+                                    justifyContent: 'flex-start',
+                                    alignItems: 'center',
+                                    background: '#001126ee',
+                                    borderRadius: 3,
+                                    padding: '1rem',
+                                }}
+                            >
+                                <Typography
+                                    variant='h6'
+                                    component='div'
+                                    sx={{
+                                        fontFamily: 'Work Sans',
+                                        fontWeight: '400',
+                                        margin: '0 1rem',
+                                    }}
+                                >
+                                    Bhargav Modak
+                                </Typography>
+                                <IconButton
+                                    href="https://github.com/your-github-username"
+                                    target="_blank"
+                                    sx={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        color: 'white',
+                                        fontSize: '3rem',
+                                        margin: '0 0.5rem',
+                                        transition: 'all 0.2s ease-in-out',
+                                        '&:hover': {
+                                            transform: 'scale(1.2)',
+                                        },
+                                    }}
+                                >
+                                    <GitHubIcon fontSize="large" />
+                                </IconButton>
+                                <IconButton
+                                    href="https://linkedin.com/in/your-linkedin-username"
+                                    target="_blank"
+                                    sx={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                        color: 'white',
+                                        fontSize: '3rem',
+                                        margin: '0 0.5rem',
+                                        transition: 'all 0.2s ease-in-out',
+                                        '&:hover': {
+                                            transform: 'scale(1.2)',
+                                        },
+                                    }}
+                                >
+                                    <LinkedInIcon fontSize="large" />
+                                </IconButton>
+
+                            </Box>
+                        </Box>
+                    </Box>
+
+
+                    <Box
+                        sx={{
+                            width: '90%',
+                            padding: '0 2rem',
+                            height: 'auto',
+                            margin: '0 auto',
+                        }}
+                    >
+                        {/* Box with 90% size of parent, rounded corners */}
+                        <Box
+                            sx={{
+                                width: '100%',
+                                height: '90%',
+                                display: 'flex',
+                                flexDirection: 'row',
+                                justifyContent: 'flex-start',
+                                alignItems: 'center',
+                                background: '#001126ee',
+                                borderRadius: 3,
+                                padding: '1rem',
+                            }}
+                        >
+                            {/* Plug to repository */}
+                            <Typography
+                                variant='h5'
+                                component='div'
+                                sx={{
+                                    fontFamily: 'Work Sans',
+                                    fontWeight: '400',
+                                    margin: '0 1rem',
+                                    display: 'flex',
+                                    alignItems: 'center',
+                                    justifyContent: 'center',
+                                }}
+                            >
+                                <GitHubIcon fontSize="large" />
+                                &nbsp;
+                                <Link
+                                    href="https://github.com/Evozone/dev-call"
+                                    target="_blank"
+                                    sx={{
+                                        color: 'white',
+                                        textDecoration: 'none',
+                                        '&:hover': {
+                                            textDecoration: 'underline',
+                                        },
+                                    }}
+                                >
+                                    Check out the repository if you want to contribute
+                                </Link>
+                            </Typography>
+
+                        </Box>
+                    </Box>
                 </Box>
+
             </Box>
-        </ThemeProvider>
+        </ThemeProvider >
     );
 };
 
---
+++ client/src/components/FeatureCard.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+
+const FeatureCard = ({ image, title, description, index }) => (
+    <Box
+        sx={{
+            width: '100%',
+            height: '500px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: index % 2 === 0 ? 'row' : 'row-reverse',
+            '@media (max-width: 600px)': {
+                flexDirection: 'column',
+            },
+        }}
+    >
+        {/* FeatureCard: Image */}
+        <Box
+            sx={{
+                width: '50%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                '@media (max-width: 600px)': {
+                    width: '100%',
+                },
+            }}
+        >
+            <img src={image} alt={title} />
+        </Box>
+        {/* FeatureCard: Text */}
+        <Box
+            sx={{
+                width: '50%',
+                px: 8,
+                '@media (max-width: 600px)': {
+                    width: '100%',
+                },
+            }}
+        >
+            <Typography variant="h3" sx={{
+                fontFamily: 'Work Sans', fontWeight: '500',
+            }}>{title}</Typography>
+            <Typography variant="body1" sx={{
+                fontFamily: 'Work Sans', fontWeight: '400', mt: 2,
+            }}
+            >{description}</Typography>
+        </Box>
+    </Box >
+);
+
+export default FeatureCard;

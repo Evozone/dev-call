a3c93c146fe0307a5db1b3162193ff754675cb8e
The Brahmnic Boy <thebrahmnicboy@gmail.com>
2023-01-07 15:09:16 +0530

    Fix dependencies and dev-dependencies.

--- client/src/__tests__/FeatureCard.test.js
+++ client/src/__tests__/FeatureCard.test.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import 'jest-styled-components';
 import FeatureCard from '../components/FeatureCard';
 
 test('renders correct image and text content', () => {
